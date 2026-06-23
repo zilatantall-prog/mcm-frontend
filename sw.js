3d7d67a4d37aa58f4143b294462032484eefe0da
Maiga Osmane <zilatantall@gmail.com>
2026-06-23 08:23:21 +0000

    Add PWA manifest and service worker

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'mcm-v1';
+const FILES = ['/', '/index.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
